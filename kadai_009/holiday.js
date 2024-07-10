eecbbb32168555b16d8a51586f6ad3e8b9c92f36
mhidano <81888041+mhidano@users.noreply.github.com>
2024-07-11 02:16:06 +0900

    Add files via upload

---
+++ kadai_009/holiday.js
@@ -0,0 +1,33 @@
+let countFor = 0;
+let countWhile = 0;
+const holidays = [
+  "正月",
+  "成人の日",
+  "建国記念日",
+  "天皇誕生日",
+  "春分の日",
+  "昭和の日",
+  "憲法記念日",
+  "みどりの日",
+  "こどもの日",
+  "海の日",
+  "山の日",
+  "敬老の日",
+  "秋分の日",
+  "スポーツの日",
+  "文化の日",
+  "勤労感謝の日",
+];
+
+let length = holidays.length;
+
+console.log("--- for ---");
+for (; countFor < length; countFor++) {
+  console.log(holidays[countFor]);
+}
+
+console.log("--- while ---");
+while (countWhile < length) {
+  console.log(holidays[countWhile]);
+  countWhile++;
+}
